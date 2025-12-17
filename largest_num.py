f3b0b0e0e9c0e879a0df909d98d74da2332ffe85
srujana <111294348+srujana523@users.noreply.github.com>
2025-12-17 20:20:53 +0530

    Add files via upload

---
+++ largest_num.py
@@ -0,0 +1,9 @@
+a = int(input("Enter 1st number:"))
+b = int(input("Enter 2nd number:"))
+c = int(input("Enter 3rd number:"))
+if a > b and a > c:
+    print("1st number is Greater")
+if b > a and b > c:
+    print("2nd number is Greater")
+else:
+    print("3rd number is Greater")
