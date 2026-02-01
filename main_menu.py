f5d2f14d3d67595fec1899eb744ce0cfacfdd3ad
Srujana523 <srujanavaradarajula52@gmail.com>
2026-02-01 18:07:16 +0530

    Added while loop

--- main_menu.py
+++ main_menu.py
@@ -1,15 +1,17 @@
 from math_basics import even_num
 from math_basics import factorial
-print("--- Welcome to Srujana's Math Tool ---")
-print("1. Check Even or Odd")
-print("2. calculate Factorial")
-
-# This line takes your input from the keyboard
-choice = input("Select an option (1 or 2): ")
-
-if choice == "1":
-    even_num.even_odd()
-elif choice == "2":
-    factorial.run_factorial()
-else:
-    print("Invalid input. Please run again and pick 1 or 2.")
+while True:
+    print("--- Welcome to Srujana's Math Tool ---")
+    print("1. Check Even or Odd")
+    print("2. calculate Factorial")
+    print("3. Exit")
+    choice = input("Select an option (1,2 or 3): ")
+    if choice == "1":
+        even_num.even_odd()
+    elif choice == "2":
+        factorial.run_factorial()
+    elif choice =="3":
+        print("Goodbye! Thanks for using my tool.")
+        break
+    else:
+        print("Invalid input. Please try again")
