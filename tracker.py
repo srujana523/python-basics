9f2dd9f475295bd5b0f90cb7c807909c07fb2096
Srujana523 <srujanavaradarajula52@gmail.com>
2026-02-03 22:06:07 +0530

    Add histoy list and session average logic

--- tracker.py
+++ tracker.py
@@ -1,9 +1,11 @@
+history = []
 while True:
     print("welcome to study log")
     law = float(input("How many hours did you prepare for lawcet today?"))
     shorthand = float(input("How many hours did you prepare for shorthand today?"))
     coding = float(input("How many hours did you prepare for coding today?"))
     total = law+shorthand+coding
+    history.append(total) #adds total to the memory list
     print(f"\nTotal study time: {total}hours")
     if total >=5:
         print("Result:Excellent you are on track")
@@ -15,4 +17,8 @@ while True:
     if stay.capitalize() == "No":
         print("Okay,Bye!")
         break
-
+print(f"your session history so far:{history}")
+average = sum(history)/len(history)
+print(f"Your average study time: {average:.2f} hours")
+print(f"\nFinal Summary: You tracked {len(history)} entries.")
+print("Keep up the great work. Bye!")
